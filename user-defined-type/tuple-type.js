d34acc8ebe9e64d0df67b173c4dc41c324325804
towhidulbu08 <towhidulislam26121997@gmail.com>
2025-01-27 07:14:01 +0600

    TS-Tutorial:07-User-Definde-Type(Tuple Type Added)

---
+++ user-defined-type/tuple-type.js
@@ -0,0 +1,5 @@
+//mixed data type: key ,value pair
+var user;
+user = [1, "John"];
+user.push(2, "Jane");
+console.log(user);
---
+++ user-defined-type/tuple-type.ts
@@ -0,0 +1,6 @@
+//mixed data type: key ,value pair
+
+let user: [number, string];
+user = [1, "John"];
+user.push(2, "Jane");
+console.log(user);
