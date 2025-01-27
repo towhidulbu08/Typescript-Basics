33345d93ae2b8d9c65951d57376aee88eec95699
towhidulbu08 <towhidulislam26121997@gmail.com>
2025-01-27 06:59:13 +0600

    TS-Tutorial:06-Union-Data-Type(Array-Type) added

---
+++ user-defined-type/array-type.js
@@ -0,0 +1,7 @@
+// let userNames: string[];
+var userNames;
+userNames = ["John", "Jane", "Jack"];
+console.log(userNames.sort());
+//let multipleTypes: (string | number)[];
+var multipleTypes;
+multipleTypes = [1, 2, 3, "John", "Jane", "Jack"];
---
+++ user-defined-type/array-type.ts
@@ -0,0 +1,10 @@
+// let userNames: string[];
+let userNames: Array<string>;
+userNames = ["John", "Jane", "Jack"];
+
+console.log(userNames.sort());
+
+//let multipleTypes: (string | number)[];
+let multipleTypes: Array<string | number>;
+
+multipleTypes = [1, 2, 3, "John", "Jane", "Jack"];
